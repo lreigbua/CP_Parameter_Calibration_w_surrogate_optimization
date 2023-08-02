ce86a1ca9a5fa257b0dc8ba9e837e6a4109dd6cf
lreigbua <luis.reig-buades@strath.ac.uk>
2023-08-02 11:55:00 +0100

    Organized_files

---
+++ src/create_materialYaml.py
@@ -0,0 +1,52 @@
+import damask
+import numpy as np
+import os
+import sys
+
+n_grains=int(sys.argv[1])
+
+cells_x=int(sys.argv[2])
+cells_y=int(sys.argv[3])
+cells_z=int(sys.argv[4])
+
+Phase_fraction_alpha=0.91
+Phase_fraction_mart=0.0
+Phase_fraction_beta=0.09
+
+
+
+# Create Material.yaml
+config_material = damask.ConfigMaterial()
+N_constituents_taylor=25
+
+config_material['homogenization']['Taylor'] = {'N_constituents':N_constituents_taylor,'mechanical':{'type':'isostrain','output': ['F','P'] }}
+config_material['phase']['Alpha'] = damask.ConfigMaterial.load('material_phase_phenopower_alpha_mod.yaml')
+config_material['phase']['Martensite'] = damask.ConfigMaterial.load('material_phase_phenopower_martensite.yaml')
+config_material['phase']['Beta'] = damask.ConfigMaterial.load('material_phase_phenopower_beta.yaml')
+
+
+oris_all=np.loadtxt('oris_all.txt')
+
+final_O=[]
+for m in range(0,n_grains):
+    oris_this_grain=damask.Rotation.from_Euler_angles(oris_all[m*25:(m*25+25),:],degrees=True)
+    final_O.append(oris_this_grain)
+    
+
+#calculate volume fractions:
+v_column=np.ones(N_constituents_taylor)
+
+v_column[0]=Phase_fraction_beta
+v_column[1:13]=Phase_fraction_mart/12
+v_column[13:25]=Phase_fraction_alpha/12
+
+
+#Create file
+config_material = config_material.material_add(phase = np.array(['Beta','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Martensite','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha','Alpha']).reshape(1,N_constituents_taylor),
+                   O=final_O,
+                   v = v_column.reshape(1,N_constituents_taylor),
+                   homogenization = 'Taylor')
+
+
+
+config_material.save()
---
+++ src/run_CP_model.m
@@ -0,0 +1,36 @@
+%close all
+
+function f=run_CP_model(param)
+%create tess
+% status = system('python create_columnar_periodic_tess.py 85 4 4 4')
+
+%modify material
+struct_params.h0 = 200.e+6*param(1);
+struct_params.xi_0_sl = [280.e+6, 325.e+6, 0.0, 365.e+6]*param(2);
+struct_params.xi_inf_sl = [568.e+6, 150.e+7, 0.0, 3420.e+6]*param(3);
+
+txt = jsonencode(struct_params,PrettyPrint=true);
+fprintf(fopen('data_json.json','wt'),txt);
+
+status = system('python modify_material.py')
+status = system('python create_materialYaml.py 85 4 4 4')
+
+%RUN MODEL
+status = system('wsl export OMP_NUM_THREADS=16 ; DAMASK_grid --load loadZ.yaml --geom Neper_columnar_85_grains.vti');
+
+
+%EXTRACT STRES STRAIN DATA PYTHON
+status = system('python plot_stress_strain.py 85 4 4 4')
+
+%PLOT STRESS STRAIN
+data = readmatrix("stress-strain_85_grains_4_cells.txt");
+
+%Removes first row to remove 0 strain data, which gives error in division
+data(1,:)=[];
+
+%Interpolate to match experimental strains
+pointstointerp = linspace(0.001,0.025,25);
+Vq=interp1(data(:,1),data(:,2),pointstointerp,'spline');
+
+f=Vq.'/1e6; %traspose and change to MPa
+end
